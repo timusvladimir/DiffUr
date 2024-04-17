7838a404a13a5cbd7e8fa543373438df75ad1e9c
TimusVladimir <timusvladimir@gmail.com>
2024-04-17 15:25:05 +0300

    other commit

--- graphs.py
+++ graphs.py
@@ -1,38 +1,39 @@
 import math
 import numpy as np
-from sympy import *
-from IPython.display import display
 import matplotlib.pyplot as plt
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator)
 
-variableA = 2;
-variableB = 2;
-variableC = 2;
-variableD = 2;
+variableA = 3
+variableB = 4
+variableC = 2
+variableD = 1
 
-sum1 = variableA + variableD;
-sum2 = variableA*variableD - variableB*variableC;
-const1 = 1;
-const2 = 2;
+sum1 = variableA + variableD
+sum2 = variableA*variableD - variableB*variableC
+const1 = 1
+const2 = 2
 
-disk = sum1 * sum1 - 4 * sum2;
+disk = sum1 * sum1 - 4 * sum2
 x_list = []
 y_list = []
 
 if disk >= 0:
-        gamma1 = (sum1 + math.sqrt(disk)) / 2;
-        gamma2 = (sum1 - math.sqrt(disk)) / 2;
-        xi1 = (gamma1 - variableA) / variableB;
-        xi2 = (gamma2 - variableA) / variableB;
+        gamma1 = (sum1 - math.sqrt(disk)) / 2
+        gamma2 = (sum1 + math.sqrt(disk)) / 2
 
-        h = 0.1;
-        tBegin = -5;
-        tEnd = 10 + h;
+        xi1 = (gamma1 - variableA) / variableB
+        xi2 = (gamma2 - variableA) / variableB
+        print (gamma1, gamma2, xi1, xi2)
+
+        h = 0.001
+        tBegin = 0
+        tEnd = 1 + h
 
         for t in np.arange(tBegin, tEnd, h):
 
-            x = (const1*math.exp(gamma1*t)+const2*math.exp(gamma2*t));
-            y = (const1*xi1*math.exp(gamma1*t)+const2*xi2*math.exp(gamma2*t));
+            x = (const1*math.exp(gamma1*t)+const2*math.exp(gamma2*t))
+            y = (const1*xi1*math.exp(gamma1*t)+const2*xi2*math.exp(gamma2*t))
+
             x_list.append(x)
             y_list.append(y)
 
@@ -43,12 +44,16 @@ if disk >= 0:
         ax.grid(which='major', linewidth=1.2)
         ax.grid(which='minor', linestyle='--', color='gray', linewidth=0.5)
 
-        ax.plot(x_list, y_list, label='')
+        ax.plot(x_list, label='')
+        ax.plot(y_list, label='')
+
        # ax.legend()
+
         ax.xaxis.set_minor_locator(AutoMinorLocator())
         ax.yaxis.set_minor_locator(AutoMinorLocator())
         ax.tick_params(which='major', length=10, width=2)
         ax.tick_params(which='minor', length=5, width=1)
         plt.show()
 
-        print(x_list, y_list)
+        print(x_list)
+        print(y_list)
